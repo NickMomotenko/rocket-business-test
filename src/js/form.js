824b529520687fb576a73fce40769f3c336404a5
nick momotenko <nickmomotenkoforweb@gmail.com>
2023-09-12 21:07:27 +0300

    fixed js files

--- src/js/form.js
+++ src/js/form.js
@@ -1,31 +1,33 @@
-const form = document.querySelector("#form");
-const formContainer = document.querySelector(".form");
+(() => {
+  const form = document.querySelector("#form");
+  const formContainer = document.querySelector(".form");
 
-const orderBtn = document.querySelectorAll(".button.button--order");
+  const orderBtn = document.querySelectorAll(".button.button--order");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const form = event.currentTarget;
-
-  const userName = form.elements.name.value;
-  const userPhone = form.elements.tel.value;
-
-  if (!userName && !userPhone) return;
-
-  alert(`Имя: ${userName} , номер телефона: ${userPhone}`);
-});
-
-formContainer.addEventListener("click", (event) => {
-  if (event.target === form || event.target.closest("#form")) return;
-
-  formContainer.style.left = "100%";
-});
-
-orderBtn.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    formContainer.style.left = "0";
+    const form = event.currentTarget;
+
+    const userName = form.elements.name.value;
+    const userPhone = form.elements.tel.value;
+
+    if (!userName && !userPhone) return;
+
+    alert(`Имя: ${userName} , номер телефона: ${userPhone}`);
   });
-});
+
+  formContainer.addEventListener("click", (event) => {
+    if (event.target === form || event.target.closest("#form")) return;
+
+    formContainer.style.left = "100%";
+  });
+
+  orderBtn.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      formContainer.style.left = "0";
+    });
+  });
+})();
